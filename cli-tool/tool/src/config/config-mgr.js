e8d015277720844779bc9c37233dc50c0de45281
phukon <rikiphukon16@gmail.com>
2023-11-16 23:46:03 +0530

    feat: schema validation -add argv error beautifier

--- cli-tool/tool/src/config/config-mgr.js
+++ cli-tool/tool/src/config/config-mgr.js
@@ -1,15 +1,40 @@
-import chalk from 'chalk'
-import { readPackageUp } from 'read-package-up'
+import chalk from 'chalk';
+import { cosmiconfigSync } from 'cosmiconfig';
+import fs from 'fs';
+import Ajv from 'ajv';
+import betterAjvErrors from 'better-ajv-errors';
+
+const ajv = new Ajv();
+const configLoader = cosmiconfigSync('tool');
+
+/*
+Import assertion are experimental. Therfore I tried loading the JSON from the filesystem synchronously.
+
+ ---Use this code beloew if you want to use import assertions---
+  import schema from './schema.json' assert { type: 'json' };
+*/
+
+const loadJSON = (path) =>
+  JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
+
+const schema = loadJSON('./schema.json');
+
 
 export async function getConfig() {
-  const pkgPath = await readPackageUp({ cwd: process.cwd() })
-  const pkg = pkgPath
-  console.log(pkg)
-  if (pkg.packageJson.tool) {
-    console.log('Found configuration')
-    return pkg.packageJson.tool
+  const result = configLoader.search(process.cwd());
+
+  if (!result) {
+    console.log(chalk.yellow('Could not find configuration, using default'));
+    return { port: 1234 };
   } else {
-    console.log(chalk.yellow('Could not find configuration, using default'))
-    return { port: 1234 }
+    const isValid = ajv.validate(schema, result.config);
+    if (!isValid) {
+      console.log(chalk.yellow('Invalid configuration was supplied'));
+      console.log();
+      console.log(betterAjvErrors(schema, result.config, ajv.errors));
+      process.exit(1);
+    }
+    console.log('Found configuration', result.config);
+    return result.config;
   }
 }
---
+++ cli-tool/tool/src/logger.js
@@ -0,0 +1,10 @@
+import chalk from 'chalk';
+
+export function createLogger(name) {
+  return {
+    log: (...args) => console.log(chalk.gray(...args)),
+    warning: (...args) => console.log(chalk.yellow(...args)),
+    highlight: (...args) => console.log(chalk.bgCyanBright(...args)),
+    debug: console.log,
+  };
+};
