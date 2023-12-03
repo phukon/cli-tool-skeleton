d5ddd039ee72aaa3d9ea946177b088a6ad26eb0e
phukon <hello@rikiphukon.com>
2023-12-03 23:04:13 +0530

    feat: Control statements

--- lgen/buffer.js
+++ lgen/buffer.js
@@ -1,78 +1,88 @@
 import select, { Separator } from '@inquirer/select';
 import input from '@inquirer/input';
 
-const URL = 'https://api.github.com/licenses'
-var entries = {}
-const ghdata = []
-var licenses = []
+const URL = 'https://api.github.com/licenses';
+const entries = {};
+const ghdata = [];
+let licenses = [];
+let isSuccess = false;
 
-fetch(URL).then((response) => {
+const fetchData = async () => {
+  try {
+    const response = await fetch(URL);
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-    return response.json(); // Parse the response body as JSON
-  }).then((d) => {
-  for (const e of d) {
- ghdata.push(e)
-}
-
-  licenses = ghdata.map(entry => ({name: entry.spdx_id, value: entry.key, description: entry.name }))
-
-}).catch(e => console.log(e))
+    const data = await response.json();
+    for (const e of data) {
+      ghdata.push(e);
+    }
+    licenses = ghdata.map(entry => ({
+      name: entry.spdx_id,
+      value: entry.key,
+      description: entry.name,
+    }));
+    isSuccess = true;
+  } catch (error) {
+    console.log('Please check your network connectivity', error);
+  }
+};
 
 const askOptions = async () => {
   try {
-    const answer = await select({
-      message: 'Select an option',
-      choices:  [
-        {
-          name: 'Auto',
-          value: 'auto',
-          description: 'Automatically generate a license using values from the package.json',
-        },
-        {
-          name: 'Custom',
-          value: 'custom',
-          description: 'Build your license yourself',
-        },
-      ],
-    });
+    await fetchData();
 
-    if (answer === 'auto') {
-      console.log('You selected Auto. Generating license...');
-      // Perform tasks related to the 'Auto' option
-    } else if (answer === 'custom') {
-      const lcs = await select({
-      message: 'Select license',
-      choices: [...licenses, new Separator()]
-    });
-      console.log('You selected:', lcs);
-      entries.license = lcs
-      if(lcs === 'bsd-2-clause' || lcs === 'bsd-3-clause' || lcs === 'mit') {
+    if (isSuccess) {
+      const answer = await select({
+        message: 'Select an option',
+        choices: [
+          {
+            name: 'Auto',
+            value: 'auto',
+            description: 'Automatically generate a license using values from the package.json',
+          },
+          {
+            name: 'Custom',
+            value: 'custom',
+            description: 'Build your license yourself',
+          },
+        ],
+      });
 
-        const nameAnswer = await input({ message: 'Enter your name' });
-        if (typeof nameAnswer !== 'string' || nameAnswer.trim().length < 1) {
+      if (answer === 'auto') {
+        console.log('You selected Auto. Generating license...');
+        // Perform tasks related to the 'Auto' option
+      } else if (answer === 'custom') {
+        const lcs = await select({
+          message: 'Select license',
+          choices: [...licenses, new Separator()],
+        });
+        console.log('You selected:', lcs);
+        entries.license = lcs;
+
+        if (lcs === 'bsd-2-clause' || lcs === 'bsd-3-clause' || lcs === 'mit') {
+          const nameAnswer = await input({ message: 'Enter your name' });
+          if (typeof nameAnswer !== 'string' || nameAnswer.trim().length < 1) {
             console.log('Invalid name. Please enter a valid name.');
             return;
           }
 
-        entries.fullname = nameAnswer
-        
-        var dateAnswer
-        var dateRegex
-        do {
-          dateAnswer = await input({ message: 'Enter the date' });
-          dateRegex = /^\d{4}$/;
-          if (!dateRegex.test(dateAnswer)) {
-            console.log('Invalid date. Please enter a valid date in YYYY format.');
-          }
+          entries.fullname = nameAnswer;
+
+          let dateAnswer;
+          const dateRegex = /^\d{4}$/;
+          do {
+            dateAnswer = await input({ message: 'Enter the date' });
+            if (!dateRegex.test(dateAnswer)) {
+              console.log('Invalid date. Please enter a valid date in YYYY format.');
+            }
           } while (!dateRegex.test(dateAnswer));
           entries.date = dateAnswer || new Date().getUTCFullYear();
-                }
-              }
+        }
+      }
+    }
 
-
-    console.log(entries)
+    console.log(entries);
   } catch (error) {
     console.error('Error occurred:', error);
   }
